e6561486d69cf0dd583156b644238fadf2b2cca6
muhammad riansyah <mriansyah93@gmail.com>
2021-02-27 08:22:02 +0700

    add notes for cutting-pdf

--- cutting-pdf.sh
+++ cutting-pdf.sh
@@ -2,11 +2,13 @@
 # jika menggunakan positional argument
 # name="$1"
 
+# gagal jika di eksekusi 
+# sh cutting-pdf.sh
 nama="Roger-S.-Pressman\;-Bruce-R.-Maxin-Software-Engineering.pdf"
 mulai=940
 sampai=963
 namabaru="pressman-referensi.pdf"
-
 pdftk $nama cat $mulai-$sampai output $namabaru
 
+# maka kopas saja kode ini ke terminal 
 pdftk Roger-S.-Pressman\;-Bruce-R.-Maxin-Software-Engineering.pdf cat 940-963 output pressman-referensi.pdf
